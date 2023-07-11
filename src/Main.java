0c11ed8573037bd9f6ec1c2151923da9651781b5
Alexander2210 <alexgaliil87@mail.ru>
2023-07-11 12:42:37 +0700

    Delete comments, correct fun and string comments

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scn = new Scanner(System.in);
         System.out.println("Input: ");
         String input = scn.nextLine();
-        String resultOut = calc(input);
-        System.out.println("Output: ");
-        System.out.println(resultOut);
+        try {
+            String resultOut = calc(input);
+            System.out.println("Output: ");
+            System.out.println(resultOut);
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Нарушение формата математической операции: два операнда и один оператор");
+            System.exit(1);
+        }
     }
 
-    public static String calc(String input) {
+    public static String calc(String input) throws NumberFormatException {
         Converter converter = new Converter();
         String[] actions = {"+", "-", "/", "*"};
         String[] regexActions = {"\\+", "-", "/", "\\*"};
@@ -26,18 +33,19 @@ public class Main {
         }
 
         if (actionIndex == -1) {
-            throw new IllegalArgumentException("Недопустимое арифметическое действие");
+            throw new IllegalArgumentException("Строка не является математической операцией");
         }
 
         String[] data = exp.split(regexActions[actionIndex]);
 
-        if (data.length > 2) {
-            throw new IllegalArgumentException("Неверный формат арифметической операции");
+        if (data.length !=2) {
+            throw new IllegalArgumentException("Нарушение формата математической операции: два операнда и один оператор");
         }
 
         if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
             int a, b;
 
+            //          try {
             boolean isRoman = converter.isRoman(data[0]);
             if (isRoman) {
                 a = Converter.romanToArabic(data[0]);
@@ -46,7 +54,13 @@ public class Main {
             } else {
                 a = Integer.parseInt(data[0]);
                 b = Integer.parseInt(data[1]);
+
             }
+//            } catch (NumberFormatException e) {
+//                System.out.println(e.getMessage());
+//                System.out.println("Нарушение формата выражения: два операнда и один оператор");
+//                System.exit(1);
+//            }
 
             if (a < 1 || a > 10 || b < 1 || b > 10) {
                 throw new IllegalArgumentException("Ошибка. Нужно вводить значения от 1 до 10");
@@ -68,7 +82,7 @@ public class Main {
 
             }
         } else {
-            throw new NumberFormatException("Числа должны быть в одном формате или Неверный формат арифметической операции");
+            throw new IllegalArgumentException("Числа должны быть в одном формате");
         }
     }
 }
